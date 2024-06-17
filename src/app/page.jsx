67e761401ce500b5e36c2bc739f20a1c9e85d106
IVEWOR <howdy@deepslog.com>
2024-06-18 03:00:00 +0530

    improved the function

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 import { useState } from "react";
 
 export default function Home() {
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(1);
 
   const randomNumberInRange = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,16 +13,13 @@ export default function Home() {
 
   const handleClick = () => {
     setNum(randomNumberInRange(1, 6));
-    document.querySelector(".hideOnClick").style.display = "none";
-    document.querySelector(".showOnClick").style.display = "block";
   };
 
   return (
     <main className="page">
       <h1>Roll a Dice</h1>
       <div className="imgWrap">
-        <Image className="hideOnClick" src={"/dice-6.png"} width="300" height="300" />
-        <Image className="showOnClick" src={"/dice-" + num + ".png"} width="300" height="300" />
+        <Image src={"/dice-" + num + ".png"} width="300" height="300" />
       </div>
 
       <div className="buttonWrap">
